69ac884f0b58715489ce6c66618072093dce9528
AlyssonVanelli <97549937+AlyssonVanelli@users.noreply.github.com>
2025-06-27 23:54:05 +0300

    Update parasha-semanal.ts

--- app/lib/parasha-semanal.ts
+++ app/lib/parasha-semanal.ts
@@ -1,4 +1,11 @@
+function getBaseUrl() {
+  return (
+    process.env.NEXT_PUBLIC_BASE_URL ||
+    (process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 'http://localhost:3000')
+  )
+}
+
 export async function getParashaSemanal() {
-  const res = await fetch(`/api/shabbat`)
+  const res = await fetch(`${getBaseUrl()}/api/shabbat`)
   return res.json()
 }
